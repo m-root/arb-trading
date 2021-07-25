e010ef988315b6fda467485e778ccf16165517a9
m-root <muemakamba@gmail.com>
2021-07-25 23:01:17 +0300

    Update and refractoring

--- engine/events.py
+++ engine/events.py
@@ -1,9 +1,79 @@
+import json
 from eth_abi import decode_abi
-from core.rpc import *
+from web3 import Web3
+from web3.contract import Contract
 
-batch_provider = BatchHTTPProvider(config[network]['http'])
-def get_reserves(pairs, blockNumber='latest'):
-    r = list(generate_get_reserves_json_rpc(pairs, blockNumber))
+from utilities.abi.uniswap_router import abi as router_abi
+from settings.settings import address_router_contract
+
+from core.rpc import generate_get_receipt_json_rpc
+from core.rpc import generate_get_reserves_json_rpc
+from core.rpc import rpc_response_batch_to_results
+from core.rpc import generate_get_allpairs_json_rpc
+from core.rpc import generate_get_pair_token01_json_rpc
+from core.rpc import BatchHTTPProvider
+from typing import List, Dict, Union
+
+from utilities.abi.uniswap_pair import abi as pair_abi
+from settings.settings import ProgramStatus
+
+def getAmountsOut(web3_ins:Web3=None, c:Contract=None, amount_in:int=None, token_address_path:List[str]=None)->List[int]:
+    if not c:
+        c = web3_ins.eth.contract(abi=router_abi.abi(), address=address_router_contract)
+    return c.functions.getAmountsOut(amount_in,token_address_path).call(block_identifier="latest")
+
+def get_allPairInfo(pairs_address:List[str], eth_http:str=None, batch_provider:BatchHTTPProvider=None,
+        web3_ins:Web3=None, blockNumber:Union[int,str]='latest', programStatus:ProgramStatus=ProgramStatus())->List[Dict]:
+
+    if eth_http:
+        web3_ins = Web3(Web3.HTTPProvider(eth_http))
+        batch_provider = BatchHTTPProvider(eth_http)
+
+    tokens0 = []
+    tokens1 = []
+    for result, fun_name in [(tokens0, "token0"), (tokens1, "token1")]:
+        r = list(generate_get_pair_token01_json_rpc(pairs_address, pair_abi.topic(fun_name), blockNumber))
+        n = len(r)
+        b = 0
+        while programStatus.running():
+            e = b+500
+            if e > n:
+                e = n
+            resp = batch_provider.make_batch_request(json.dumps(r[b:e]))
+            for ad in rpc_response_batch_to_results(resp):
+                result.append(Web3.toChecksumAddress(ad[-40:]))
+            b = e
+            if b >= n:
+                break
+
+    return [{"address":pair, "token0":tokens0[i], "token1":tokens1[i]} for i,pair in enumerate(pairs_address)]
+
+def get_allPairAddress(eth_http:str=None, batch_provider:BatchHTTPProvider=None,
+        web3_ins:Web3=None, blockNumber:Union[int,str]='latest', programStatus:ProgramStatus=ProgramStatus())->List[str]:
+
+    if eth_http:
+        web3_ins = Web3(Web3.HTTPProvider(eth_http))
+        batch_provider = BatchHTTPProvider(eth_http)
+
+    r = list(generate_get_allpairs_json_rpc(web3_ins, blockNumber))
+    n = len(r)
+    b = 0
+    results = []
+    while programStatus.running():
+        e = b+500
+        if e > n:
+            e = n
+        resp = batch_provider.make_batch_request(json.dumps(r[b:e]))
+        for ad in rpc_response_batch_to_results(resp):
+            results.append(Web3.toChecksumAddress(ad[-40:]))
+        b = e
+        if b >= n:
+            break
+
+    return results
+
+def get_reserves(web3_ins:Web3, batch_provider:BatchHTTPProvider, pairs:List[Dict], blockNumber:Union[int,str]='latest')->List[Dict]:
+    r = list(generate_get_reserves_json_rpc(web3_ins, pairs, blockNumber))
     resp = batch_provider.make_batch_request(json.dumps(r))
     results = list(rpc_response_batch_to_results(resp))
     for i in range(len(results)):
@@ -12,35 +82,8 @@ def get_reserves(pairs, blockNumber='latest'):
         pairs[i]['reserve1'] = res[1]
     return pairs
 
-def get_receipts(txhashes):
+def get_receipts(batch_provider:BatchHTTPProvider, txhashes:List[str])->List[Dict]:
     receipts_rpc = list(generate_get_receipt_json_rpc(txhashes))
     resp = batch_provider.make_batch_request(json.dumps(receipts_rpc))
     results = rpc_response_batch_to_results(resp)
     return list(results)
-
-def get_block_txhashes(blockNumber='latest'):
-    block = w3.eth.getBlock(blockNumber)
-    txhashes = [t.hex() for t in block['transactions']]
-    return txhashes
-
-sync_topic = "0x1c411e9a96e071241c2f21f7726b17ae89e3cab4c78be50e062b03a9fffbbad1"
-swap_topic = "0xd78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822"
-mint_topic = "0x4c209b5fc8ad50758f13e2e1088ba56a560dff690a1c6fef26394f4c03821c4f"
-burn_topic = "0xdccd412f0b1252819cb1fd330b93224ca42612892bb3f4f789976e6d81936496"
-def process_receipts(receipts, pairsDict, pairs):
-    c = w3.eth.contract(abi=pairABI)
-    for r in receipts:
-        if len(r['logs']) == 0:
-            continue
-        for log in r['logs']:
-            if log['topics'][0] not in [sync_topic]:
-                continue
-            if log['address'] not in pairsDict.keys():
-                continue
-            # process event
-            event = None
-            if log['topics'][0] == sync_topic:
-                event = c.events.Sync().processLog(log)
-                pairs[pairsDict[log['address']]['arrIndex']]['reserve0'] = event['reserve0']
-                pairs[pairsDict[log['address']]['arrIndex']]['reserve1'] = event['reserve1']
-    return pairs
