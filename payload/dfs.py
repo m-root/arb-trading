112158282bcb65ac9821bc6416f01485a1a6e2f5
m-root <muemakamba@gmail.com>
2021-07-26 08:51:32 +0300

    DFS update on finding arbitrage opportunities

--- payload/dfs.py
+++ payload/dfs.py
@@ -1,70 +1,62 @@
-from core.common import *
+from decimal import Decimal
+from typing import List, Dict
 
-'''
-# path = [tokenIn]
-def findBestTradeExactIn(pairs, tokenIn, tokenOut, amountIn, maxHops, currentPairs, path, origToken, origAmount, bestTrade):
-    for i in range(len(pairs)):
-        pair = pairs[i]
-        if not pair['token0']['address'] == tokenIn['address'] and not pair['token1']['address'] == tokenIn['address']:
+from core.common import getOptimalAmount
+from core.common import getEaEb
+from core.common import getAmountOut, getAmountOutByPath
+from settings.settings import ProgramStatus
+
+def findArb(pairs:List[Dict], tokenIn:Dict, tokenOut:Dict, maxHops:int, currentPairs:List, path:List,
+        bestTrades:List[Dict], programStatus:ProgramStatus=ProgramStatus(), count:int=5)->List[Dict]:
+
+    len_path = len(path)
+
+    for i,pair in enumerate(pairs):
+        if not programStatus.running():
+            return bestTrades
+
+        if not pair:
             continue
-        if pair['reserve0'] == 0 or pair['reserve1'] == 0:
-            continue
-        if tokenIn['address'] == pair['token0']['address']:
-            amountOut = getAmountOut(amountIn, pair['reserve0'], pair['reserve1'])
-            tempOut = pair['token0']
-        if tokenIn['address'] == pair['token1']['address']:
-            amountOut = getAmountOut(amountIn, pair['reserve1'], pair['reserve0'])
-            tempOut = pair['token1']
-        path.append(tempOut)
-        if tempOut == tokenOut:
-            newTrade = { 'route': currentPairs + [pair], 'path': path, 'tokenIn': origToken, 'tokenOut': tokenOut, 'amountIn': origAmount, 'amountOut': amountOut }
-            if not bestTrade:
-                bestTrade = newTrade
-            if bestTrade['amountOut'] < amountOut:
-                bestTrade = newTrade
-        elif maxHops > 1 and len(pairs) > 1:
-            pairsExcludingThisPair = pairs[:i] + pairs[i+1:]
-            findBestTradeExactIn(pairsExcludingThisPair, tempOut, tokenOut, amountOut, maxHops-1, currentPairs + [pair], path, origToken, origAmount, bestTrade)
-        return bestTrade
-'''
 
-
-def sortTrades(trades, newTrade):
-    trades.append(newTrade)
-    return sorted(trades, key=lambda x: x['profit'])
-
-
-# path = [tokenIn]
-def findArb(pairs, tokenIn, tokenOut, maxHops, currentPairs, path, bestTrades, count=5):
-    for i in range(len(pairs)):
         newPath = path.copy()
-        pair = pairs[i]
         if not pair['token0']['address'] == tokenIn['address'] and not pair['token1']['address'] == tokenIn['address']:
             continue
-        if pair['reserve0'] / pow(10, pair['token0']['decimal']) < 1 or pair['reserve1'] / pow(10, pair['token1'][
-            'decimal']) < 1:
+
+        if pair['reserve0']/pow(10, pair['token0']['decimal']) < 1 or pair['reserve1']/pow(10, pair['token1']['decimal']) < 1:
             continue
+
         if tokenIn['address'] == pair['token0']['address']:
             tempOut = pair['token1']
         else:
             tempOut = pair['token0']
+
         newPath.append(tempOut)
-        if tempOut['address'] == tokenOut['address'] and len(path) > 2:
-            Ea, Eb = getEaEb(tokenOut, currentPairs + [pair])
-            newTrade = {'route': currentPairs + [pair], 'path': newPath, 'Ea': Ea, 'Eb': Eb}
+        newCurrentPairs = currentPairs + [pair]
+
+        if tempOut['address'] == tokenOut['address'] and len_path > 2:
+            Ea, Eb = getEaEb(tokenOut, newCurrentPairs)
             if Ea and Eb and Ea < Eb:
-                newTrade['optimalAmount'] = getOptimalAmount(Ea, Eb)
-                if newTrade['optimalAmount'] > 0:
-                    newTrade['outputAmount'] = getAmountOut(newTrade['optimalAmount'], Ea, Eb)
-                    newTrade['profit'] = newTrade['outputAmount'] - newTrade['optimalAmount']
-                    newTrade['p'] = int(newTrade['profit']) / pow(10, tokenOut['decimal'])
-                else:
+                optA = getOptimalAmount(Ea, Eb)
+                if optA <= 0:
                     continue
-                bestTrades = sortTrades(bestTrades, newTrade)
-                bestTrades.reverse()
+
+                status, outA = getAmountOutByPath(tokenOut, optA, newCurrentPairs)
+                if not status:
+                    continue
+
+                profit = outA[-1] - outA[0]
+                newTrade = {
+                        "route": newCurrentPairs,
+                        "path": newPath,
+                        "amountsOut": outA,
+                        "profit": profit,
+                        }
+                bestTrades.append(newTrade)
+                bestTrades.sort(key = lambda x: x["profit"], reverse=True)
                 bestTrades = bestTrades[:count]
         elif maxHops > 1 and len(pairs) > 1:
-            pairsExcludingThisPair = pairs[:i] + pairs[i + 1:]
-            bestTrades = findArb(pairsExcludingThisPair, tempOut, tokenOut, maxHops - 1, currentPairs + [pair], newPath,
-                                 bestTrades, count)
+            pairs[i] = None
+            bestTrades = findArb(pairs, tempOut, tokenOut, maxHops-1, newCurrentPairs, newPath, bestTrades, programStatus, count)
+            pairs[i] = pair
+
     return bestTrades
