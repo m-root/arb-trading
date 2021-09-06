8effd43d31911c5e3ea4043a157c4c84c05d25d0
m-root <muemakamba@gmail.com>
2021-09-06 13:23:45 +0300

    RPC

--- core/rpc.py
+++ core/rpc.py
@@ -1,5 +1,5 @@
 from web3 import HTTPProvider
-from web3._utils.request import make_post_request
+from web3._utils.request import make_post_request, async_make_post_request
 from settings.settings import *
 from utilities.abi.uniswap_pair import abi as pair_abi
 from settings.settings import address_factory_contract
@@ -15,6 +15,7 @@ def generate_json_rpc(method, params, request_id=1):
         'id': request_id,
     }
 
+
 def generate_get_block_by_number_json_rpc(block_numbers, include_transactions):
     for idx, block_number in enumerate(block_numbers):
         yield generate_json_rpc(
@@ -23,9 +24,11 @@ def generate_get_block_by_number_json_rpc(block_numbers, include_transactions):
             request_id=idx
         )
 
+
 class RetriableValueError(Exception):
     def __init__(self, error_message):
         self.__error_message = error_message
+
     def __str__(self):
         return self.__error_message
 
@@ -34,14 +37,15 @@ def generate_get_reserves_json_rpc(pairs, blockNumber='latest'):
     c = web3_ins.eth.contract(abi=factory_abi.abi())
     for pair in pairs:
         yield generate_json_rpc(
-                method='eth_call',
-                params=[{
-                    'to': pair['address'],
-                    'data': c.encodeABI(fn_name='getReserves', args=[]),
-                    },
-                    hex(blockNumber) if blockNumber != 'latest' else 'latest',
-                    ]
-                )
+            method='eth_call',
+            params=[{
+                'to': pair['address'],
+                'data': c.encodeABI(fn_name='getReserves', args=[]),
+            },
+                hex(blockNumber) if blockNumber != 'latest' else 'latest',
+            ]
+        )
+
 
 def generate_get_receipt_json_rpc(transaction_hashes):
     for idx, transaction_hash in enumerate(transaction_hashes):
@@ -50,6 +54,7 @@ def generate_get_receipt_json_rpc(transaction_hashes):
             params=[transaction_hash],
             request_id=idx
         )
+
 
 def rpc_response_batch_to_results(response):
     for response_item in response:
@@ -66,10 +71,11 @@ def rpc_response_to_result(response):
             # synced node
             raise RetriableValueError(error_message)
         elif response.get('error') is not None:
-        # elif response.get('error') is not None and is_retriable_error(response.get('error').get('code')):
+            # elif response.get('error') is not None and is_retriable_error(response.get('error').get('code')):
             raise RetriableValueError(error_message)
         raise ValueError(error_message)
     return result
+
 
 class BatchHTTPProvider(HTTPProvider):
 
@@ -78,23 +84,26 @@ class BatchHTTPProvider(HTTPProvider):
                           self.endpoint_uri, text)
         request_data = text.encode('utf-8')
         print('----------------------------')
-        print(request_data)
+        # print(request_data)
+        print(self.get_request_kwargs())
         print('-----------------------------')
 
         # print(self.get_request_kwargs())
         # print((request_data))
-        raw_response = make_post_request(
+        raw_response = async_make_post_request(
             # self.endpoint_uri,
-           "https://mainnet.infura.io/v3/3ccfb5a8cc7349239102c9600827044b",
+            # "https://mainnet.infura.io/v3/3ccfb5a8cc7349239102c9600827044b",
+            "http://127.0.0.1:8545",
             request_data,
             **self.get_request_kwargs()
         )
-        print(raw_response)
+        print(type(raw_response))
         response = self.decode_rpc_response(raw_response)
         self.logger.debug("Getting response HTTP. URI: %s, "
                           "Request: %s, Response: %s",
                           self.endpoint_uri, text, response)
         return response
+
 
 # from web3 import HTTPProvider
 # from web3._utils.request import make_post_request
@@ -121,45 +130,50 @@ class BatchHTTPProvider(HTTPProvider):
 #             request_id=idx
 #         )
 #
-def generate_get_pair_token01_json_rpc(pairs_address:List[str], token01_fun_topic:str, blockNumber:Union[int,str]='latest')->Iterable[Dict]:
+def generate_get_pair_token01_json_rpc(pairs_address: List[str], token01_fun_topic: str,
+                                       blockNumber: Union[int, str] = 'latest') -> Iterable[Dict]:
     for pair in pairs_address:
         yield generate_json_rpc(
-                method='eth_call',
-                params=[{
-                    'to': pair,
-                    'data': '{}'.format(token01_fun_topic[0:10]),
-                    },
-                    hex(blockNumber) if blockNumber != 'latest' else 'latest',
-                    ]
-                )
+            method='eth_call',
+            params=[{
+                'to': pair,
+                'data': '{}'.format(token01_fun_topic[0:10]),
+            },
+                hex(blockNumber) if blockNumber != 'latest' else 'latest',
+            ]
+        )
 
-def generate_get_allpairs_json_rpc(web3_ins:Web3, blockNumber:Union[int,str]='latest')->Iterable[Dict]:
+
+def generate_get_allpairs_json_rpc(web3_ins: Web3, blockNumber: Union[int, str] = 'latest') -> Iterable[Dict]:
     c2 = web3_ins.eth.contract(abi=factory_abi.abi(), address=address_factory_contract)
     topic = factory_abi.topic("allPairs")
     num = c2.functions.allPairsLength().call()
     for n in range(0, num):
         yield generate_json_rpc(
-                method='eth_call',
-                params=[{
-                    'to': address_factory_contract,
-                    'data': '{}{:0>64s}'.format(topic[0:10], hex(n)[2:]),
-                    },
-                    hex(blockNumber) if blockNumber != 'latest' else 'latest',
-                    ]
-                )
+            method='eth_call',
+            params=[{
+                'to': address_factory_contract,
+                'data': '{}{:0>64s}'.format(topic[0:10], hex(n)[2:]),
+            },
+                hex(blockNumber) if blockNumber != 'latest' else 'latest',
+            ]
+        )
 
-def generate_get_reserves_json_rpc(web3_ins:Web3, pairs:List[Dict], blockNumber:Union[int,str]='latest')->Iterable[Dict]:
+
+def generate_get_reserves_json_rpc(web3_ins: Web3, pairs: List[Dict], blockNumber: Union[int, str] = 'latest') -> \
+Iterable[Dict]:
     c = web3_ins.eth.contract(abi=pair_abi.abi())
     for pair in pairs:
         yield generate_json_rpc(
-                method='eth_call',
-                params=[{
-                    'to': pair['address'],
-                    'data': c.encodeABI(fn_name='getReserves', args=[]),
-                    },
-                    hex(blockNumber) if blockNumber != 'latest' else 'latest',
-                    ]
-                )
+            method='eth_call',
+            params=[{
+                'to': pair['address'],
+                'data': c.encodeABI(fn_name='getReserves', args=[]),
+            },
+                hex(blockNumber) if blockNumber != 'latest' else 'latest',
+            ]
+        )
+
 
 # def generate_get_receipt_json_rpc(transaction_hashes:List[str])->Iterable[Dict]:
 #     for idx, transaction_hash in enumerate(transaction_hashes):
